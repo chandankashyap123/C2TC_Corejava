8dd5ae758eb4a6f4280c8fa325113c997e1ceb5f
Chandan c v <Chandan c v@DESKTOP-P43VGBR>
2022-09-15 18:46:41 +0530

    Fisrt Commit

---
+++ B25_corejava13/src/com/tns/prog1/Natural.java
@@ -0,0 +1,13 @@
+package com.tns.prog1;
+
+public class Natural 
+{
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number;
+for(number=0;number<=10;number++) {
+	System.out.println(number);
+}
+	}
+
+}
