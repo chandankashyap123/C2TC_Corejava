02f58e7d263dc307600001081855f80b40094674
Chandan c v <Chandan c v@DESKTOP-P43VGBR>
2022-09-15 18:59:43 +0530

    First commit

---
+++ B25_corejava6/src/com/tns/prog/FormatTIme24HoursExample.java
@@ -0,0 +1,23 @@
+package com.tns.prog;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+	public class FormatTIme24HoursExample {
+
+		public static void main(String[] args) {
+
+			// Getting the current date and time
+			Date currentDate = new Date();
+			
+			// Creating simple date formatter to 24 hours
+			SimpleDateFormat formatter = new SimpleDateFormat("kk:mm:ss");
+			
+			// getting the time in 24 hours format
+			String timeIn24Hours = formatter.format(currentDate);
+			
+			// printing the time
+			System.out.println("Current time in 24 hours format : "+timeIn24Hours);
+		}
+	}
+
+
