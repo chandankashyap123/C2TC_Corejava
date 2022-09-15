88ce54c9b8a45349a6a609b6229f5b184fb92d02
Chandan c v <Chandan c v@DESKTOP-P43VGBR>
2022-09-15 18:45:12 +0530

    First commit

---
+++ B25_corejava12/src/com/tns/prog1/Temp.java
@@ -0,0 +1,19 @@
+package com.tns.prog1;
+import java.util.Scanner;
+public class Temp 
+{
+	public static void main(String[] args)// TODO Auto-generated method stub
+	{
+		
+		Scanner sc=new Scanner(System.in);
+		int temperature=sc.nextInt();;
+		int week=5;
+		for(int i=1;i<week;i++) 
+		{
+			temperature=temperature-2;
+			System.out.println(temperature);
+		}
+
+	}
+
+}
