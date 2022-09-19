33067bdb24512382f40eb047b4697225424531a4
Chandan c v <Chandan c v@DESKTOP-P43VGBR>
2022-09-19 13:07:04 +0530

    first commit

---
+++ B25_corejava17/src/assingment_2/Construct_student.java
@@ -0,0 +1,12 @@
+package assingment_2;
+
+public class Construct_student {
+	Construct_student(){
+		System.out.println("Student object is created");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Construct_student xc=new Construct_student();
+	}
+
+}
